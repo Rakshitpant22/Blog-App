15edbbd8e2ca77b20d73ae48cd22d10e058c9ca8
Rakshitpant22 <rakshitpant22@gmail.com>
2024-10-02 13:56:05 +0530

    Footer created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 
@@ -22,6 +23,7 @@ export default function App() {
       <Route path="/Projects" element={<Projects/>}/>
 
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
