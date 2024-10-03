9737030d08d76edca9394260c8cb7e64524cd919
Rakshitpant22 <rakshitpant22@gmail.com>
2024-10-03 19:13:49 +0530

    completed Dark theme

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice'
 import { configureStore,combineReducers } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
     user : userReducer,
+    theme: themeReducer,
 });
 
 const persistConfig={
