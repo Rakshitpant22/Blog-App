37e3004aeffddf43725ef8750c0c59a019e6d040
Rakshitpant22 <rakshitpant22@gmail.com>
2024-10-06 13:34:39 +0530

    about page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      
+
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Sahand' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Rakshit's Blog! This blog was created by Rakshit Pant
+              as a personal project to share his thoughts and ideas with the
+              world.  
+            </p>
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as software engineering, and programming
+              languages. I am  always learning and exploring new
+              technologies!
+            </p>
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              my users. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-
